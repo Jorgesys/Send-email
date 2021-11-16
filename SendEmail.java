92e58c0d0ebf65cb050afcfbc82d3108bb76efba
Jorgesys <jorge.osorio@gmail.com>
2021-11-16 14:50:39 -0600

    Create SendEmail.java

---
+++ SendEmail.java
@@ -0,0 +1,14 @@
+
+
+        String[] addresses = new String[]{"jorgesys12@gmail.com, jorgesys@paypal.com"};
+        String subject = "Hi, how is it going";
+        String message = "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. https://es.stackoverflow.com/users/95/";
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL, addresses);
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message );
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
